77f1118e1f0cda6c18ba82aa0e155d89792d5aa3
Hyunsuk Jang <lluniquell@gaonpay.com>
2023-02-23 09:20:22 +0900

    20230920 JHS

---
+++ src/main/java/com/gaon/nifou/v3/ctrl/sub02/sub02_07.java
@@ -0,0 +1,34 @@
+package com.gaon.nifou.v3.ctrl.sub02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.gaon.nifou.v3.trans_ora_manager;
+
+@WebServlet("/sub02/07.gaon")
+public class sub02_07 extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		trans_ora_manager oram = new trans_ora_manager();
+		JSONArray jsonary = new JSONArray();
+		PrintWriter out = response.getWriter();
+
+		String orgcd = request.getParameter("orgcd");
+		String pages = request.getParameter("pages");
+
+		out.print(oram.get_sub0207(oram.get_tb_sys_domain(orgcd, pages)));
+
+	}
+}
